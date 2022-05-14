b92a7233a25bb5521fb1496ea132b0c87a867da1
geekbim <abimanyu.gatya@salt.co.id>
2022-05-14 18:49:30 +0700

    fix(jwt): fix jwt check

--- pkg/service/jwt/service.go
+++ pkg/service/jwt/service.go
@@ -64,6 +64,9 @@ func (j *jwtService) GenerateToken(UserID string) string {
 }
 
 func (j *jwtService) ValidateToken(token string) (*jwt.Token, error) {
+	if len(strings.Split(token, "Bearer ")) > 0 {
+		return nil, fmt.Errorf("token not found")
+	}
 	token = strings.Split(token, "Bearer ")[1]
 	return jwt.Parse(token, func(t_ *jwt.Token) (interface{}, error) {
 		if _, ok := t_.Method.(*jwt.SigningMethodHMAC); !ok {
