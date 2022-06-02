6859933e467a2b574475600ca6f440bb75d26cc8
geekbim <abimanyu.gatya@salt.co.id>
2022-06-02 21:26:53 +0700

    refactor(project): refactor product query

--- internal/repository/psql/helper/product_repository_helper.go
+++ internal/repository/psql/helper/product_repository_helper.go
@@ -2,9 +2,12 @@ package helper
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"gokomodo/domain/entity"
 	"gokomodo/internal/repository/psql/mapper"
 	"gokomodo/internal/repository/psql/models"
+	"gokomodo/pkg/common"
 
 	"github.com/rocketlaunchr/dbq/v2"
 )
@@ -20,4 +23,17 @@ func StoreProduct(ctx context.Context, E dbq.EFn, product *entity.Product) error
 	}
 
 	return nil
+}
+
+func FindProductById(ctx context.Context, db *sql.DB, productId common.ID) (*entity.Product, error) {
+	stmt := fmt.Sprintf(`SELECT * FROM %s WHERE id = $1`, models.Product{}.TableName())
+
+	opts := &dbq.Options{DecoderConfig: dbq.StdTimeConversionConfig()}
+
+	results, err := dbq.Qs(ctx, db, stmt, models.Product{}, opts, productId.String())
+	if err != nil {
+		return nil, err
+	} else {
+		return mapper.ToDomainProduct(results.(*models.Product)), err
+	}
 }
