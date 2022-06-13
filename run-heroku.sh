a194b5a1a306b78559de5fbc49f68e6b8ef5c2eb
geekbim <manyuabim9@gmail.com>
2022-06-13 07:34:47 +0700

    build(go-rest-wa): add bin file

--- run-heroku.sh
+++ run-heroku.sh
@@ -1,5 +1,5 @@
 go build -o bin/go_wa_rest -v cmd/main.go
 git add .
 git commit -m "build(go-rest-wa): add bin file"
-git push origin master
+git push origin development
 git push heroku development:master
