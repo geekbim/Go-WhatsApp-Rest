8fa85b3d73d9b1414f9a7a07605a7cc1b89fdd7c
geekbim <manyuabim9@gmail.com>
2022-06-13 07:35:01 +0700

    initial commit

--- run-heroku.sh
+++ run-heroku.sh
@@ -1,5 +1,5 @@
 go build -o bin/go_wa_rest -v cmd/main.go
 git add .
-git commit -m "build(go-rest-wa): add bin file"
+git commit -m "initial commit"
 git push origin development
 git push heroku development:master
