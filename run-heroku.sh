783af5b9732399abf21f61b42b7ff4eebe32e521
Abim <abim@Abims-MacBook-Pro.local>
2023-03-10 20:42:17 +0700

    refactor: delete heroku run script

--- run-heroku.sh
+++
@@ -1,6 +0,0 @@
-# heroku config:set BUILDPACK_URL=https://github.com/kr/heroku-buildpack-go
-go build -o bin/go_wa_rest -v cmd/main.go
-git add .
-git commit -m "initial commit"
-git push origin master
-git push heroku master:master
